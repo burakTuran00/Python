6cf5a0f7844db39ceae878dcb4a9227c2dd6835a
burakTuran00 <burakturan7410@gmail.com>
2022-12-21 21:57:38 +0300

    Python

--- Python_Project/PyhonProject/calculator.py
+++ Python_Project/PyhonProject/calculator.py
@@ -1,10 +1,16 @@
-def main():
-    x = int(input("What's x: "))
-    print("x squared is",square(x))
+x  = float(input("What's x: "));
+y = float(input("What's y: "));
 
+sum = x + y;
+sub = x - y;
+div = x / y;
+pow = x * y;
 
+print("x + y = ",sum);
+print("x - y = ",sub);
+print("x / y = ",div);
+print("x * y = ",pow);
 
-main()
 
 
 
